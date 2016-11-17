28029b535483a5fbe632e57a3c78be06e490dbec
Jim <Jim@DESKTOP-0VIFM9N>
2016-11-16 19:21:53 -0800

    Naming

--- DataStructuresDoneWrong/Launchpad.cs
+++ DataStructuresDoneWrong/Launchpad.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DataStructuresDoneWrong {
-    class Program {
+    class Launchpad {
         static void Main(string[] args) {
             //nothing
         }
