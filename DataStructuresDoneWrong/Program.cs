0014950bccfe0c47edbe4fd1b508b34f205c9b43
Jim <Jim@DESKTOP-0VIFM9N>
2016-11-16 19:18:26 -0800

    Test commit

--- DataStructuresDoneWrong/Program.cs
+++ DataStructuresDoneWrong/Program.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructuresDoneWrong
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace DataStructuresDoneWrong {
+    class Program {
+        static void Main(string[] args) {
+            //nothing
         }
     }
 }
