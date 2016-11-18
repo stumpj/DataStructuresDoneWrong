792bcf30527e3bd4efc4f8bb06f9245b30e0f8cb
Jim <Jim@DESKTOP-0VIFM9N>
2016-11-17 22:43:48 -0800

    More methods implemented

--- DataStructuresDoneWrong/LL/JimLL.cs
+++ DataStructuresDoneWrong/LL/JimLL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructuresDoneWrong.LL {
 
-    public class JimLL<T> : BadLinkedList<T> {
+    public class JimLL<T> : BadLinkedList<T> where T : IEquatable<T>, ICloneable {
 
         private Node Head, Tail;
         private int ListSize;
@@ -60,58 +60,111 @@ namespace DataStructuresDoneWrong.LL {
             ListSize = 0;
         }
 
-        public T Clone() {
-            throw new NotImplementedException();
-        }
-
         public bool Contains(T value) {
             return Contains(value, Head);
         }
 
         public T Get(int index) {
-            throw new NotImplementedException();
+            if (index < 0 || index > ListSize) {
+                throw new ArgumentOutOfRangeException("index", "Index is out of range");
+            } else {
+                return Get(Head, index);
+            }
         }
 
         public T GetFirst() {
-            throw new NotImplementedException();
+            if (IsEmpty) {
+                throw new Exception("The list is empty");
+            } else {
+                return Head.Value;
+            }
         }
 
         public T GetLast() {
-            throw new NotImplementedException();
+            if (IsEmpty) {
+                throw new Exception("The list is empty");
+            } else {
+                return Tail.Value;
+            }
         }
 
         public int IndexOf(T value) {
-            throw new NotImplementedException();
+            return IndexOf(value, Head, 0);
         }
 
         public int LastIndexOf(T value) {
-            throw new NotImplementedException();
+            return LastIndexOf(value, Tail, ListSize - 1);
         }
 
         public bool Remove(T value) {
-            throw new NotImplementedException();
+            int index = IndexOf(value);
+            if (index == -1) {
+                return false;
+            } else {
+                Remove(index);
+                return true;
+            }
         }
 
         public T Remove(int index) {
-            throw new NotImplementedException();
+            if (index < 0 || index >= ListSize) {
+                throw new ArgumentOutOfRangeException("index", "Index is out of range");
+            } if (index == 0) {
+                return RemoveFirst();
+            } else if(index == ListSize - 1) {
+                return RemoveLast();
+            } else {
+                ListSize--;
+                return Remove(index, Head);
+            }
         }
 
         public T RemoveFirst() {
-            throw new NotImplementedException();
+            T removed = Head.Value;
+            if (IsEmpty) {
+                throw new Exception("The list is already empty");
+            } else if (ListSize == 1) {
+                Head = null;
+                Tail = null;
+            } else {
+                Head = Head.Next;
+                Head.Prev = null;
+            }
+            ListSize--;
+            return removed;
         }
 
         public T RemoveLast() {
-            throw new NotImplementedException();
+            T removed = Tail.Value;
+            if (IsEmpty) {
+                throw new Exception("The list is already empty");
+            } else if (ListSize == 1) {
+                Head = null;
+                Tail = null;
+            } else {
+                Tail = Tail.Prev;
+                Tail.Next = null;
+            }
+            ListSize--;
+            return removed;
         }
 
         public T Set(int index, T value) {
-            throw new NotImplementedException();
+            if (index < 0 || index >= ListSize) {
+                throw new ArgumentOutOfRangeException("index", "Index is out of range");
+            } else {
+                return Set(index, value, Head);
+            }
         }
 
         public T[] ToArray() {
             T[] listArray = new T[ListSize];
             ToArray(listArray, Head, 0);
             return listArray;
+        }
+
+        public object Clone() {
+            throw new NotImplementedException();
         }
 
         private void AddFirstValue(T value) {
@@ -152,6 +205,54 @@ namespace DataStructuresDoneWrong.LL {
             if (cur != null) {
                 listArray[index] = cur.Value;
                 ToArray(listArray, cur.Next, index + 1);
+            }
+        }
+
+        private int IndexOf(T value, Node cur, int index) {
+            if (cur == null) {
+                return -1;
+            } else if (cur.Value.Equals(value)) {
+                return index;
+            } else {
+                return IndexOf(value, cur.Next, index + 1);
+            }
+        }
+
+        private int LastIndexOf(T value, Node cur, int index) {
+            if (cur == null) {
+                return -1;
+            } else if (cur.Value.Equals(value)) {
+                return index;
+            } else {
+                return IndexOf(value, cur.Prev, index - 1);
+            }
+        }
+
+        private T Get(Node cur, int index) {
+            if (index == 0) {
+                return cur.Value;
+            } else {
+                return Get(cur.Next, index + 1);
+            }
+        }
+
+        private T Set(int index, T value, Node cur) {
+            if (index == 0) {
+                T oldValue = cur.Value;
+                cur.Value = value;
+                return oldValue;
+            } else {
+                return Set(index + 1, value, cur.Next);
+            }
+        }
+
+        private T Remove(int index, Node cur) {
+            if (index == 0) {
+                cur.Prev = cur.Next;
+                cur.Next = cur.Prev;
+                return cur.Value;
+            } else {
+                return Remove(index - 1, cur.Next);
             }
         }
 
